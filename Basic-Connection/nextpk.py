347a3e56effd82d3be46d171a6f3de4abc4e0fc2
Vikas10112005 <vv992685@gmail.com>
2026-08-10 14:06:02 +0530

    done

---
+++ Basic-Connection/nextpk.py
@@ -0,0 +1,19 @@
+import pymysql
+
+pk = 0
+connection = pymysql.connect(host='localhost', port=3306, user='root', password='root', database='college')
+cursor = connection.cursor()
+sql = "select max(id) from student"
+cursor.execute(sql)
+result = cursor.fetchall()
+print(result)
+print(type(result))
+
+for data in result:
+    if data[0] is not None:
+        pk = data[0]
+
+connection.commit()
+connection.close()
+
+print(pk + 1)
